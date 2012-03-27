d84259969d187ec39561607ca543de1cb0d8d337
Badri Narayan Bhaskar <badrinarayan@gmail.com>
2012-03-26 19:25:53 -0500

    Include SAST in analysis and change submit file

--- analyze.m
+++ analyze.m
@@ -1,11 +1,13 @@
 function [mmse_ast, mmse_cadzow,mse_var_ast,mse_var_cadzow,mean_k_est] = analyze(n,k,nl)
 	fileNo = 120*(log(n/100)/log(2)) + 8*(k-5) + 10*(log(nl)/log(2)-1);
 	ast_mses    = [];
+	sast_mses   = [];
 	cadzow_mses = [];
 	k_ests      = [];
 	%fprintf('fileNos = %d to %d\n',(fileNo+1),(fileNo+10));
 	for idx=fileNo+1:fileNo+10
-		load(strcat('output',int2str(idx),'.mat'))
+		load(strcat('output',int2str(idx),'.mat'));
+		sast_mses = [sast_mses;sast_mse];
 		ast_mses = [ast_mses;ast_mse];
 		if n > 500
 			cadzow_mse = Inf;
@@ -17,9 +19,10 @@ function [mmse_ast, mmse_cadzow,mse_var_ast,mse_var_cadzow,mean_k_est] = analyze
 		%fprintf('ast_mse = %.4f\n',ast_mse);
 	end
 	mmse_ast = mean(ast_mses);
+	mmse_sast = mean(sast_mses);
 	mmse_cadzow = mean(cadzow_mses);
 	mse_var_ast = var(ast_mses);
 	mse_var_cadzow = var(cadzow_mses);
 	mean_k_est = mean(k_ests);
-	fprintf('mse(ast) = %.4f, mse(cadzow)= %.4f, mean k_est = %.2f\n',mmse_ast, mmse_cadzow,mean_k_est);
+	fprintf('mse(ast) = %.4f, mse(sast) = %.4f, mse(cadzow)= %.4f, mean k_est = %.2f\n',mmse_ast, mmse_sast, mmse_cadzow,mean_k_est);
 end
--- performance_profiling.m
+++ performance_profiling.m
@@ -1,23 +1,26 @@
 function performance_profiling(experiment)
 % Plot the performance profile
 	ast_mses = [];
+	sast_mses = [];
 	cadzow_mses = [];
 	for fileNo = 1:360
 		fileName = strcat('outputs/',experiment,'/output',int2str(fileNo),'.mat');
 		load(fileName);
 		ast_mses = [ast_mses; ast_mse];
+		sast_mses = [sast_mses; sast_mse];
 		cadzow_mses = [cadzow_mses; cadzow_mse];
 	end
-	r_min = min([ast_mses'; cadzow_mses']);
+	r_min = min([ast_mses'; cadzow_mses'; sast_mses']);
 	r_ast = ast_mses./r_min';
+	r_sast = sast_mses./r_min';
 	r_cadzow = cadzow_mses./r_min';
-	t_max = ceil(max([r_ast;r_cadzow]));
+	t_max = ceil(max([r_ast;r_cadzow;r_sast]));
 	t_max = 10;
 	t = linspace(1,t_max);
-	plot(t,arrayfun(@(x)sum(r_ast<x)/360,t),'k-',t,arrayfun(@(x)sum(r_cadzow<x)/360,t),'k--','LineWidth',1.5);
+	plot(t,arrayfun(@(x)sum(r_ast<x)/360,t),'k-',t,arrayfun(@(x)sum(r_sast<x)/360,t),'k:',t,arrayfun(@(x)sum(r_cadzow<x)/360,t),'k--','LineWidth',1.5);
 	axis([1 t_max 0 1])
 	%[h,childObjs] = legend('DAST','Cadzow','Location','SouthEast');
-	[h,childObjs] = legend('      DAST','      Cadzow');
+	[h,childObjs] = legend('      DAST','      SAST','      Cadzow');
 	hx = xlabel('\beta');
 	hy = ylabel('P(\beta)');
 	legend(gca,'boxoff');
