f260c3f8211588407b8197f4325e9288da6204fa
Pallavika Ramaswamy <pallavika87+git@gmail.com>
2012-03-28 16:46:11 -0500

    Correct Toeplitz Approx

--- toeplitz_approx.m
+++ toeplitz_approx.m
@@ -1,12 +1,12 @@
-function T = toeplitz_approx(A)
+function Ta = toeplitz_approx(A)
 % TOEPLITZ_APPROX(A)
 % For a square matrix A, find the Toeplitz approximation
 % by averaging along the diagonals.
-N = length(A);
-T = zeros(size(A));
-for n = -(N-1):(N-1)
-    d = diag(A,n);
-    d(:) = mean(d);
-    T = T + diag(d,n);
+[M,N] = size(A);
+Ta = zeros(size(A));
+b = zeros(M+N-1,1);
+for n = -(M-1):(N-1)
+		b(M+n) = mean(diag(A,n));
 end
+Ta = T(b)
 end
