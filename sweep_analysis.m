cb38e88451a503155f3207b8cfd8a1ff97a20fdb
Pallavika Ramaswamy <pallavika87+git@gmail.com>
2012-03-30 13:56:07 -0500

    Add Sweep Analysis MSE Ratio

--- sweep_analysis.m
+++ sweep_analysis.m
@@ -9,6 +9,7 @@ for algorithm={'ast','dast'}
 		fprintf('%s\n',algorithm{1});
 		fprintf('%s\n',experiment{1});
 		z_opt = containers.Map();
+		ratio = containers.Map();
 		for fileNo = 1:480
 			filename = sprintf('outputs/sweep_%s/%s%d.mat',experiment{1},algorithm{1},fileNo);
 			if ~exist(filename)
@@ -20,14 +21,17 @@ for algorithm={'ast','dast'}
 			key = sprintf('%d_%d_%d',n,k,nl);
 			if isKey(z_opt,key)
 				z_opt(key) = [z_opt(key);data.z_opt];
+				ratio(key) = [ratio(key);data.min_mse/data.base_mse];
 			else
 				z_opt(key) = [data.z_opt];
+				ratio(key) = [data.z_opt];
 			end
 		end 
 		% Display the means in a table
 		for n=ns,for k=ks,for nl=nls
 			key = sprintf('%d_%d_%d',n,k,nl);
-			fprintf('n = %d, k = %d, nl = %d, z_opt = %f\n',n,k,nl,mean(z_opt(key)));
+			%fprintf('n = %d, k = %d, nl = %d, z_opt = %f\n',n,k,nl,mean(z_opt(key)));
+			fprintf('n = %d, k = %d, nl = %d, ratio = %f\n',n,k,nl,mean(ratio(key)));
 		end; end; end
 	end
 end
