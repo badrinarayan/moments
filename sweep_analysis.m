a6a24ab06b9890870864fe4a32c7768d883405e8
Pallavika Ramaswamy <pallavika87+git@gmail.com>
2012-03-30 13:11:35 -0500

    Sweep Analysis

--- sweep_analysis.m
+++ sweep_analysis.m
@@ -1,11 +1,34 @@
 zs   = [logspace(-0.5,0.5,8) 1];
 ns   = [100,200,400,800];
 ks   = [5,10,15];
-nls  = [2 4 8 16];
+nls  = [2,4,8,16];
 L    = Linearize(ns,ks,nls);
-zOpt = containers.Map()
-for fileNo = 1:480
-  data = load(sprintf('inputs/random/input%d.txt',fileNo));
-  [n,k,nl] = L.values(fileNo);
-  z_opt{[n,k,l]} = data.z_opt;
-end 
+for algorithm={'ast','dast'}
+	for experiment={'random','equispaced'}
+		% Read in z_opt's
+		fprintf('%s\n',algorithm{1});
+		fprintf('%s\n',experiment{1});
+		z_opt = containers.Map();
+		for fileNo = 1:480
+			filename = sprintf('outputs/sweep_%s/%s%d.mat',experiment{1},algorithm{1},fileNo);
+			if ~exist(filename)
+				fprintf('%s does not exist\n');
+				continue
+			end
+			data = load(filename);
+			[n,k,nl] = L.values(fileNo);
+			key = sprintf('%d_%d_%d',n,k,nl);
+			if isKey(z_opt,key)
+				z_opt(key) = [z_opt(key);data.z_opt];
+			else
+				z_opt(key) = [data.z_opt];
+			end
+		end 
+		% Display the means in a table
+		for n=ns,for k=ks,for nl=nls
+			key = sprintf('%d_%d_%d',n,k,nl);
+			fprintf('n = %d, k = %d, nl = %d, z_opt = %f\n',n,k,nl,mean(z_opt(key)));
+		end; end; end
+	end
+end
+
