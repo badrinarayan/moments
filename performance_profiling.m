2327528b829fc1908c0ec4c83004ade7030f2e91
Pallavika Ramaswamy <pallavika87+git@gmail.com>
2012-03-31 22:06:41 -0500

    New Plots

--- performance_profiling.m
+++ performance_profiling.m
@@ -9,35 +9,32 @@ function performance_profiling(experiment)
 	for fileNo = 1:480
 		fileName = strcat('outputs/',experiment,'/output',int2str(fileNo),'.mat');
 		load(fileName);
-		fileName = strcat('outputs/',experiment,'/sweep_dast',int2str(fileNo),'.mat');
-		load(fileName);
+		%fileName = strcat('outputs/',experiment,'/sweep_dast',int2str(fileNo),'.mat');
+		%load(fileName);
 		ast_mses = [ast_mses; ast_mse];
-		ast_mses15 = [ast_mses15; ast_mse15];
-		ast_mses16 = [ast_mses16; ast_mse16];
-		ast_mses17 = [ast_mses17; ast_mse17];
+		%ast_mses15 = [ast_mses15; ast_mse15];
+		%ast_mses16 = [ast_mses16; ast_mse16];
+		%ast_mses17 = [ast_mses17; ast_mse17];
 		sast_mses = [sast_mses; sast_mse];
 		cadzow_mses = [cadzow_mses; cadzow_mse];
 	end
 	r_min = min([ast_mses'; cadzow_mses'; sast_mses']);
 	r_ast = ast_mses./r_min';
-	r_ast15 = ast_mses15./r_min';
-	r_ast16 = ast_mses16./r_min';
-	r_ast17 = ast_mses17./r_min';
+	%r_ast15 = ast_mses15./r_min';
+	%r_ast16 = ast_mses16./r_min';
+	%r_ast17 = ast_mses17./r_min';
 	r_sast = sast_mses./r_min';
 	r_cadzow = cadzow_mses./r_min';
 	t_max = ceil(max([r_ast;r_cadzow;r_sast]));
 	t_max = 4;
 	t = linspace(1,t_max);
 	plot(...
-				t,arrayfun(@(x)sum(r_sast<x)/480,t),...
-				t,arrayfun(@(x)sum(r_ast15<x)/480,t),...
-				t,arrayfun(@(x)sum(r_ast16<x)/480,t),...
-				t,arrayfun(@(x)sum(r_ast17<x)/480,t),...
-				t,arrayfun(@(x)sum(r_cadzow<x)/480,t)...
+				t,arrayfun(@(x)sum(r_sast<x)/480,t),'k-',...
+				t,arrayfun(@(x)sum(r_ast<x)/480,t),'k--',...
+				t,arrayfun(@(x)sum(r_cadzow<x)/480,t),'k-.'...
 	);
-	%t,arrayfun(@(x)sum(r_ast<x)/480,t),...
 	axis([1 t_max 0 1])
-	[h,childObjs] = legend('        AST','        DAST 2^{15}','        DAST 2^{16}','        DAST 2^{17}','        Cadzow');
+	[h,childObjs] = legend('        AST','        DAST', '        Cadzow','Location','SouthEast');
 	hx = xlabel('\beta');
 	hy = ylabel('P(\beta)');
 	legend(gca,'boxoff');
@@ -53,6 +50,7 @@ function performance_profiling(experiment)
 		elseif length(xCoords{lineIdx}) == 2,
 			set( lineObjs(lineIdx), 'XData', xCoords{lineIdx} + [0 0.15]) ;
 		end
+		set(lineObjs(lineIdx),'LineWidth',1.5)
 	end
 	% title('Performance Profile')
 	saveas(gcf,strcat('figures/',experiment,'_pp.pdf'),'pdf');
--- sweep_dast.m
+++ sweep_dast.m
@@ -12,31 +12,33 @@ fprintf('Received MSE=%.4f\n',e(received)^2);
 
 % Gridding Algorithm:
 tic;
-[grid,grid_c,grid_debiased15,grid_c_debiased] = moment_sparsa(received,tau,2^15);
-ast_time15 = toc;
-ast_mse15  = e(grid_debiased15)^2;
+[grid,grid_c,grid_debiased12,grid_c_debiased] = moment_sparsa(received,tau,2^12);
+ast_time12 = toc;
+ast_mse12  = e(grid_debiased12)^2;
 
-disp(['(2^15) Atomic Softthreshold Time : ' num2str(ast_time15)]);
-fprintf('Gridding MSE=%.4f\n',ast_mse15);
-
-tic;
-[grid,grid_c,grid_debiased16,grid_c_debiased] = moment_sparsa(received,tau,2^16);
-ast_time16 = toc;
-ast_mse16  = e(grid_debiased16)^2;
-
-disp(['(2^16) Atomic Softthreshold Time : ' num2str(ast_time16)]);
-fprintf('Gridding MSE=%.4f\n',ast_mse16);
+disp(['(2^12) Atomic Softthreshold Time : ' num2str(ast_time12)]);
+fprintf('Gridding MSE=%.4f\n',ast_mse12);
 
 tic;
-[grid,grid_c,grid_debiased17,grid_c_debiased] = moment_sparsa(received,tau,2^17);
-ast_time17 = toc;
-ast_mse17  = e(grid_debiased17)^2;
+[grid,grid_c,grid_debiased13,grid_c_debiased] = moment_sparsa(received,tau,2^13);
+ast_time13 = toc;
+ast_mse13  = e(grid_debiased13)^2;
 
-disp(['(2^17) Atomic Softthreshold Time : ' num2str(ast_time17)]);
-fprintf('Gridding MSE=%.4f\n',ast_mse17);
+disp(['(2^13) Atomic Softthreshold Time : ' num2str(ast_time13)]);
+fprintf('Gridding MSE=%.4f\n',ast_mse13);
+
+tic;
+[grid,grid_c,grid_debiased14,grid_c_debiased] = moment_sparsa(received,tau,2^14);
+ast_time14 = toc;
+ast_mse14  = e(grid_debiased14)^2;
+
+disp(['(2^14) Atomic Softthreshold Time : ' num2str(ast_time14)]);
+fprintf('Gridding MSE=%.4f\n',ast_mse14);
 
 % Save to file
-save(outfile,'ast_mse15','ast_mse16','ast_mse17',...
-'ast_time15','ast_time16','ast_time17')
+if exist(outfile)
+	save(outfile,'ast_mse12','ast_mse13','ast_mse14',...
+	'ast_time12','ast_time13','ast_time14','-append')
+end
 
 end
